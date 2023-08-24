6bec8671f0b42d4e2ff133754e8a7ab3b15eeb51
noobmaster69jr <srashwin22@gmail.com>
2023-08-24 07:43:34 +0530

    bfs shortest path

---
+++ BreadthFirstSearch/BfsShortestPath.java
@@ -0,0 +1,95 @@
+package BreadthFirstSearch;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BfsShortestPath {
+
+	static class Graph {
+		int V;
+		ArrayList<Integer>[] list;
+
+		public Graph(int v) {
+			V = v;
+			list = new ArrayList[v];
+			for (int i = 0; i < v; i++) {
+				list[i] = new ArrayList<>();
+			}
+		}
+
+		void addEdge(int i, int j, boolean unDirected) {
+			list[i].add(j);
+			if (unDirected)
+				list[j].add(i);
+		}
+
+		void bfs(int source, int dest) {
+			Queue<Integer> q = new LinkedList<>();
+			Boolean[] visited = new Boolean[V];
+			Arrays.fill(visited, false);
+			int dist[] = new int[V];
+			int parent[] = new int[V];
+
+			for (int i = 0; i < V; i++) {
+				parent[i] = -1;
+				dist[i] = 0;
+			}
+
+			q.add(source);
+			visited[source] = true;
+			parent[source] = source;
+			dist[source] = 0;
+
+			while (!q.isEmpty()) {
+				int val = q.poll();
+
+				// if neighbours are not visited add them to queue
+				for (int i : list[val]) {
+					if (!visited[i]) {
+						q.add(i);
+						parent[i] = val;
+						dist[i] = dist[val] + 1;
+						visited[i] = true;
+					}
+				}
+
+			}
+
+			// single source shortest path algorithm
+
+			// shortest distance to all nodes from the source
+			for (int i = 0; i < V; i++) {
+				System.out.println("Shortest dist to " + i + " is " + dist[i]);
+
+			}
+
+			// printing shortest path to a destination
+			if (dest != -1) {
+				int temp = dest;
+				System.out.print("\nShortest path to " + dest + " from " + " source is ");
+				while (temp != source) {
+					System.out.print(temp + " --> ");
+					temp = parent[temp];
+				}
+				System.out.print(source);
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Graph g = new Graph(7);
+		g.addEdge(0, 1, true);
+		g.addEdge(1, 2, true);
+		g.addEdge(2, 3, true);
+		g.addEdge(3, 5, true);
+		g.addEdge(5, 6, true);
+		g.addEdge(4, 5, true);
+		g.addEdge(0, 4, true);
+		g.addEdge(3, 4, true);
+		g.bfs(1, 5);
+
+	}
+}
